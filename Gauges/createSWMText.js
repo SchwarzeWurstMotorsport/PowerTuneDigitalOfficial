af1c78a792dfc42bb7bc27650aa0650e4715719c
bellcham <bellcham@gmail.com>
2025-09-22 18:02:59 +1000

    add extra params to creation js

--- Gauges/createSWMText.js
+++ Gauges/createSWMText.js
@@ -3,18 +3,18 @@ var component;
 var gauge;
 
 
-function createText(setX,setY,setdisplaytext,setfonttype,setfontsize,settextcolor,setbackgroundcolor,setdatasourcename,setfontbold,setdecimalpoints) {
+function createText(setX,setY,setdisplaytext,setfonttype,setfontsize,settextcolor,setbackgroundcolor,setdatasourcename,setfontbold,setdecimalpoints, setthreshold_1, setthreshold_2, setthreshold_3, setregion_1_color, setregion_2_color, setregion_3_color, setregion_4_color) {
     console.log("Qt.createComponent SWM text label ");
     component = Qt.createComponent("SWM_TextLabel.qml");
     if (component.status === Component.Ready){
         console.log("SWM text label component ready");
-        finishCreation(setX,setY,setdisplaytext,setfonttype,setfontsize,settextcolor,setbackgroundcolor,setdatasourcename,setfontbold,setdecimalpoints);
+        finishCreation(setX,setY,setdisplaytext,setfonttype,setfontsize,settextcolor,setbackgroundcolor,setdatasourcename,setfontbold,setdecimalpoints, setthreshold_1, setthreshold_2, setthreshold_3, setregion_1_color, setregion_2_color, setregion_3_color, setregion_4_color);
     }
      else
         component.statusChanged.connect(finishCreation);
 }
 
-function finishCreation(setX,setY,setdisplaytext,setfonttype,setfontsize,settextcolor,setbackgroundcolor,setdatasourcename,setfontbold,setdecimalpoints) {
+function finishCreation(setX,setY,setdisplaytext,setfonttype,setfontsize,settextcolor,setbackgroundcolor,setdatasourcename,setfontbold,setdecimalpoints, setthreshold_1, setthreshold_2, setthreshold_3, setregion_1_color, setregion_2_color, setregion_3_color, setregion_4_color) {
     if (component.status === Component.Ready) {
         console.log("finishCreation SWM text label ");
         gauge = component.createObject(userDash, {
@@ -28,7 +28,14 @@ function finishCreation(setX,setY,setdisplaytext,setfonttype,setfontsize,settext
                                         "datasourcename": setdatasourcename,
                                         "fontbold" :setfontbold,
                                         "decimalpoints" :setdecimalpoints,
-                                        "backgroundcolor": setbackgroundcolor
+                                        "backgroundcolor": setbackgroundcolor,
+                                        "threshold_1": setthreshold_1,
+                                        "threshold_2": setthreshold_2,
+                                        "threshold_3": setthreshold_3,
+                                        "region_1_color": setregion_1_color,
+                                        "region_2_color": setregion_2_color,
+                                        "region_3_color": setregion_3_color,
+                                        "region_4_color": setregion_4_color
                                         });
 
         if (gauge === null) {
